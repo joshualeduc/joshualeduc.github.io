8dd16685878363f621100f7281ad058acec8456f
Joshua LeDuc <joshualeduc@gmail.com>
2016-03-11 16:20:13 -0700

    testimonial slider added

--- js/init.js
+++ js/init.js
@@ -4,6 +4,7 @@
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
     $('.scrollspy').scrollSpy();
+    $('.slider').slider({height: 200, full_width: true});
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
