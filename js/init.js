50bc7ffdcf84d2f49a9a05a436a4c8eedd99515f
Joshua LeDuc <joshualeduc@gmail.com>
2016-03-12 14:38:22 -0700

    fade

--- js/init.js
+++ js/init.js
@@ -6,5 +6,13 @@
     $('.scrollspy').scrollSpy();
     $('.slider').slider({height: 200, full_width: true});
 
+    var options = [
+    {selector: '#fade1', offset: 200, callback: 'Materialize.fadeInImage("#fade1")'},
+    {selector: '#fade2', offset: 200, callback: 'Materialize.fadeInImage("#fade2")'},
+    {selector: '#fade3', offset: 200, callback: 'Materialize.fadeInImage("#fade3")'},
+    {selector: '#fade4', offset: 200, callback: 'Materialize.fadeInImage("#fade4")'}
+    ];
+    Materialize.scrollFire(options);
+
   }); // end of document ready
 })(jQuery); // end of jQuery name space
