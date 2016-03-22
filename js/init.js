134859d89e909ddc347a79ae67d5fe7c214548fd
Joshua LeDuc <joshualeduc@outlook.com>
2016-03-21 21:12:03 -0600

    deleted comments

--- js/init.js
+++ js/init.js
@@ -8,9 +8,6 @@
 
     var options = [
     {selector: '#fade1', offset: 150, callback: 'Materialize.fadeInImage("#fade1")'}
-    // {selector: '#fade2', offset: 150, callback: 'Materialize.fadeInImage("#fade2")'},
-    // {selector: '#fade3', offset: 150, callback: 'Materialize.fadeInImage("#fade3")'},
-    // {selector: '#fade4', offset: 150, callback: 'Materialize.fadeInImage("#fade4")'}
     ];
     Materialize.scrollFire(options);
 
